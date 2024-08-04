42515b1cd732f7d564fb1286e8ca4ce55b37350d
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-03 22:12:56 -0300

    refactor: add not null validations

--- backend/app/intmed_api/models/user.py
+++ backend/app/intmed_api/models/user.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class User(models.Model):
     id: int = models.AutoField(primary_key=True)
-    name: str = models.CharField(max_length=100)
-    email: str = models.EmailField(unique=True)
-    password: str = models.CharField(max_length=50)
+    name: str = models.CharField(max_length=100, blank=False, null=False)
+    email: str = models.EmailField(unique=True, blank=False, null=False)
+    password: str = models.CharField(max_length=50, blank=False, null=False)
 
     def __str__(self) -> str:
         return self.name
