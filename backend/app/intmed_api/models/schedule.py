0b314db3940aa0232b168863c1f59023a9a805ea
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-03 22:32:12 -0300

    feat: implement schedule model

---
+++ backend/app/intmed_api/models/schedule.py
@@ -0,0 +1,31 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils import timezone
+
+from .doctor import Doctor
+
+
+class Schedule(models.Model):
+    id: int = models.AutoField(primary_key=True)
+    doctor: Doctor = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE, related_name="schedules", null=False
+    )
+    day: date = models.DateField()
+
+    class Meta:
+        unique_together = ("doctor", "day")
+
+    def NoSaveScheduleWithPassDate(self):
+        if self.day < timezone.now().date():
+            raise ValidationError(
+                "Não é possível criar uma agenda para um dia passado."
+            )
+
+    def save(self, *args, **kwargs):
+        self.NoSaveScheduleWithPassDate()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.doctor.name} - {self.day}"
