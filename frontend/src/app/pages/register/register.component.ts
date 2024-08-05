39716312f508e0cd888b2e03712fd1c83222710f
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-05 17:35:10 -0300

    feat[FRONTEND]: implement register page

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, inject } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckbox} from '@angular/material/checkbox';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule ,
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCheckbox
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm!: FormGroup
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+      passwordConfirm: ['', Validators.required]
+    });
+  }
+
+  onCancel() {
+    this.router.navigate(['/login'])
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      const { username, password} = this.registerForm.value;
+       console.log({ username, password});
+    } else {
+      this.registerForm.markAllAsTouched()
+    }
+  }
 }
