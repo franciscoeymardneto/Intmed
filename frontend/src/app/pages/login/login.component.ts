fd9f561194b0bdf8451e536ae0bd851554061f0a
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-05 16:23:23 -0300

    feat[FRONTEND]: implement login page

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckbox} from '@angular/material/checkbox';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule ,
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCheckbox
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+
+  onLogin() {
+
+    if (this.loginForm.valid) {
+      const { username, password, rememberMe } = this.loginForm.value;
+       console.log({ username, password, rememberMe });
+    } else {
+      this.loginForm.markAllAsTouched()
+    }
+  }
+
+  onRegister() {
+    this.router.navigate(['/register'])
+  }
 }
