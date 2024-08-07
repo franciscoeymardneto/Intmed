98712bafb2e38e298b9aa107a04ab9febe1b15c6
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-07 14:02:54 -0300

    test: ensure the crm field at the doctor model will be unique

--- backend/app/intmed_api/tests/models/doctor_spec.py
+++ backend/app/intmed_api/tests/models/doctor_spec.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from ...models import Doctor, Speciality
@@ -23,9 +24,26 @@ class DoctorModelTestSuit(TestCase):
     def test_str_method(self):
         # Testa o método __str__ do modelo
         doctor = Doctor.objects.create(
-            name="Dr. Edward Richtofen",
-            crm="12345678-9/CE",
-            email="dr.ed.richtofen@intmed.com",
+            name="Dr. Ludwig Maxis",
+            crm="98765432-1/CE",
+            email="dr.lud.maxis@intmed.com",
             speciality=self.speciality,
         )
-        self.assertEqual(str(doctor), "Dr. Edward Richtofen - 12345678-9/CE")
+        self.assertEqual(str(doctor), "Dr. Ludwig Maxis - 98765432-1/CE")
+
+    def test_unique_crm(self):
+        # Testa o unique do campo crm
+        Doctor.objects.create(
+            name="Dr. Stephen Strange",
+            crm="12345678-9/CE",
+            email="dr.stephen.strange@example.com",
+            speciality=self.speciality,
+        )
+        with self.assertRaises(ValidationError):
+            doctor = Doctor(
+                name="Dra. Maria Oliveira",
+                crm="12345678-9/CE",
+                email="dr.maria.oliveira@example.com",
+                speciality=self.speciality,
+            )
+            doctor.full_clean()
