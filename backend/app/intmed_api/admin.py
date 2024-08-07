ec8e5708277f7949b330bc58172112cc7b59ac71
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-06 22:32:32 -0300

    feat: implement specialities model and add it to doctor model

--- backend/app/intmed_api/admin.py
+++ backend/app/intmed_api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Doctor, Schedule, ScheduleHour
+from .models import Doctor, Schedule, ScheduleHour, Speciality
 
 admin.site.register(Doctor)
 admin.site.register(ScheduleHour)
 admin.site.register(Schedule)
+admin.site.register(Speciality)
