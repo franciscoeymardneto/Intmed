920d10939f842f5dc1c57ac6af3968ee7ecd59ad
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-07 13:52:31 -0300

    fix: increase length of the crm field

--- backend/app/intmed_api/models/doctor.py
+++ backend/app/intmed_api/models/doctor.py
@@ -6,7 +6,7 @@ from .speciality import Speciality
 class Doctor(models.Model):
     id: int = models.AutoField(primary_key=True)
     name: str = models.CharField(max_length=100, blank=False, null=False)
-    crm: str = models.CharField(max_length=10, unique=True, blank=False, null=False)
+    crm: str = models.CharField(max_length=15, unique=True, blank=False, null=False)
     email: str = models.EmailField()
     speciality = models.ForeignKey(
         Speciality,
