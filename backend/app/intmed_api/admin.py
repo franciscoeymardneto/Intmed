7626401e2acfb34ec9d66632a186ed583328c42a
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-06 21:32:11 -0300

    feat: register api models to admin user

--- backend/app/intmed_api/admin.py
+++ backend/app/intmed_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doctor, Schedule, ScheduleHour
+
+admin.site.register(Doctor)
+admin.site.register(ScheduleHour)
+admin.site.register(Schedule)
