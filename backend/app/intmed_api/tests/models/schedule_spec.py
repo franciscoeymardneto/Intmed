ec28e2cb1b92dfe8605967dacb8d771baaf813e2
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-07 15:17:15 -0300

    test: ensure that the ListConsultFromThisSchedule method returns all consults from this schedule

--- backend/app/intmed_api/tests/models/schedule_spec.py
+++ backend/app/intmed_api/tests/models/schedule_spec.py
@@ -8,6 +8,12 @@ from ...models import Doctor, Schedule, Speciality
 
 class ScheduleModelTestSuit(TestCase):
     currentTimezone = timezone.localtime(timezone.now())
+    hours = [
+        (currentTimezone + timedelta(minutes=1)).time(),
+        (currentTimezone + timedelta(hours=1)).time(),
+        (currentTimezone + timedelta(hours=2)).time(),
+        (currentTimezone + timedelta(hours=3)).time(),
+    ]
 
     def setUp(self):
         self.doctor = Doctor.objects.create(
@@ -19,34 +25,30 @@ class ScheduleModelTestSuit(TestCase):
 
     def test_create_schedule(self):
         # Testa a criação de uma agenda com todos os campos válidos
-
-        hours = [
-            (self.currentTimezone + timedelta(minutes=1)).time(),
-            (self.currentTimezone + timedelta(hours=1)).time(),
-            (self.currentTimezone + timedelta(hours=2)).time(),
-            (self.currentTimezone + timedelta(hours=3)).time(),
-        ]
         schedule = Schedule.objects.create(
-            doctor=self.doctor, day=self.currentTimezone.date(), hours=hours
+            doctor=self.doctor, day=self.currentTimezone.date(), hours=self.hours
         )
 
         self.assertEqual(schedule.doctor, self.doctor)
         self.assertEqual(schedule.day, self.currentTimezone.date())
-        self.assertEqual(schedule.hours, hours)
+        self.assertEqual(schedule.hours, self.hours)
 
     def test_str_method(self):
         # Testa o método __str__ do modelo
-        hours = [
-            (self.currentTimezone + timedelta(minutes=1)).time(),
-            (self.currentTimezone + timedelta(hours=1)).time(),
-            (self.currentTimezone + timedelta(hours=2)).time(),
-            (self.currentTimezone + timedelta(hours=3)).time(),
-        ]
         schedule = Schedule.objects.create(
-            doctor=self.doctor, day=self.currentTimezone.date(), hours=hours
+            doctor=self.doctor, day=self.currentTimezone.date(), hours=self.hours
         )
-
         self.assertEqual(
             str(schedule),
             f"Dr. Edward Richtofen - {self.currentTimezone.date().strftime("%d/%m/%Y")}"
         )
+
+    def test_list_consult_from_this_schedule(self):
+        # Testa o método ListConsultFromThisSchedule
+        consult_time = (self.currentTimezone + timedelta(minutes=1)).time()
+        schedule = Schedule.objects.create(
+            doctor=self.doctor, day=self.currentTimezone.date(), hours=self.hours
+        )
+        from ...models import Consult
+        Consult.objects.create(schedule=schedule, hour=consult_time)
+        self.assertIn(consult_time, schedule.ListConsultFromThisSchedule())
