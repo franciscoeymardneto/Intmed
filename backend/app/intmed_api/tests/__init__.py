a62b1975d90c13f02cff237c94eac43bf106cbc4
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-07 22:17:34 -0300

    test: ensure that user login serializer and user login response serializer will work successfully

--- backend/app/intmed_api/tests/__init__.py
+++ backend/app/intmed_api/tests/__init__.py
@@ -2,3 +2,5 @@ from .models.consult_spec import CosultModelTestSuit  # noqa: F401
 from .models.doctor_spec import DoctorModelTestSuit  # noqa: F401
 from .models.schedule_spec import ScheduleModelTestSuit  # noqa: F401
 from .models.speciality_spec import SpecialityModelTestSuit  # noqa: F401
+from .serializers.user_login_serializer_spec import (  # noqa: F401
+    UserLoginResponseSerializerTestSuit, UserLoginSerializerTestSuit)
