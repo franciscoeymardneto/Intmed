e907413548138cacf0b9007f4a4bd0864a4e917b
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-08 14:29:14 -0300

    feat: implement list doctors by speciality endpoint

--- backend/app/intmed_api/urls.py
+++ backend/app/intmed_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import (createUser, list_consults, list_schedules, list_speciality,
-                    login)
+from .views import (createUser, list_consults, list_doctors, list_schedules,
+                    list_speciality, login)
 
 urlpatterns = [
     path("especialidades", list_speciality, name="speciality"),
     path("consultas", list_consults, name="list_consults"),
+    path("medicos", list_doctors, name="list_doctors"),
     path("agendas", list_schedules, name="list_schedules"),
     path("users", createUser, name="register"),
     path("users/login", login, name="login"),
--- backend/app/intmed_api/views/__init__.py
+++ backend/app/intmed_api/views/__init__.py
@@ -1,4 +1,5 @@
 from .consult_list_view import list_consults  # noqa: F401
+from .doctor_list_view import list_doctors  # noqa: F401
 from .schedule_list_view import list_schedules  # noqa: F401
 from .speciality_list_view import list_speciality  # noqa: F401
 from .user_login_view import login  # noqa: F401
