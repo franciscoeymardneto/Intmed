bbec1b476533da9b5632541abd4a82b1b3029b27
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-08 19:31:03 -0300

    feat: implement speciality service

---
+++ frontend/src/app/core/models/speciality.ts
@@ -0,0 +1,4 @@
+export type Speciality = {
+  id: number,
+  nome: string
+}
---
+++ frontend/src/app/core/services/speciality.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpService } from './http.service';
+import { ApiSpecialityResponse } from '../dto/api/speciality.api.dto';
+import { Speciality } from '../models/speciality';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpecialityService {
+  constructor(private http: HttpService) {
+  }
+
+  list(): Observable<Speciality[]> {
+    return this.http.get<ApiSpecialityResponse[]>(`/especialidades`).pipe(
+      map( response => {
+        return response
+      })
+    )
+  }
+}
