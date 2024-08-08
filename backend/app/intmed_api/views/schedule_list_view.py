2efd98b3e9be0d7ffae36d147c48557b5918daaf
franciscoeymardneto <francisco.eymard.oliveira@gmail.com>
2024-08-08 14:12:50 -0300

    feat: implement schedule list with all list bussiness rules

--- backend/app/intmed_api/views/schedule_list_view.py
+++ backend/app/intmed_api/views/schedule_list_view.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+from django.db.models import Q
 from django.utils import timezone
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -45,12 +47,55 @@ from ..serializers import ScheduleSerializer
 )
 @api_view(["GET"])
 def list_schedules(request):
+    now = timezone.localtime(timezone.now())
+    today = now.date()
+    current_time = now.time()
+
+    filters = {}
+
+    try:
+        filters = define_filters(request, filters)
+    except ValidationError as error:
+        return Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+    queryset = Schedule.objects.filter(
+        Q(day__gte=today),
+        Q(hours__len__gt=0),
+        **filters,
+    ).order_by("day")
+
+    serializer = ScheduleSerializer(remove_past_hours(queryset), many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def remove_past_hours(queryset):
+    now = timezone.localtime(timezone.now())
+    today = now.date()
+    current_time = now.time()
+    new_object_list = []
+
+    for obj in queryset:
+        if obj.day == today:
+            new_hours = [hour for hour in obj.hours if hour >= current_time]
+            if new_hours:
+                new_object = Schedule(
+                    id=obj.id, day=obj.day, doctor=obj.doctor, hours=new_hours
+                )
+                new_object_list.append(new_object)
+        else:
+            new_object = Schedule(
+                id=obj.id, day=obj.day, doctor=obj.doctor, hours=obj.hours
+            )
+            new_object_list.append(new_object)
+
+    return new_object_list
+
+
+def define_filters(request, filters):
     medico_ids = request.query_params.getlist("medico", [])
     crm_numbers = request.query_params.getlist("crm", [])
     data_inicio = request.query_params.get("data_inicio", None)
     data_final = request.query_params.get("data_final", None)
-
-    filters = {}
 
     if medico_ids:
         filters["doctor__id__in"] = medico_ids
@@ -64,18 +109,14 @@ def list_schedules(request):
             start_date = timezone.datetime.strptime(data_inicio, "%Y-%m-%d").date()
             end_date = timezone.datetime.strptime(data_final, "%Y-%m-%d").date()
             if start_date > end_date:
-                return Response(
-                    {"error": "Data de início deve ser anterior ou igual a Data final"},
-                    status=status.HTTP_400_BAD_REQUEST,
+                raise ValidationError(
+                    "Data de início deve ser anterior ou igual a Data final.",
+                    code="invalid_date",
                 )
-
             filters["day__range"] = [start_date, end_date]
         except ValueError:
-            return Response(
-                {"error": "Formato de data inválido"},
-                status=status.HTTP_400_BAD_REQUEST,
+            raise ValidationError(
+                "Formato de data inválido.", code="invalid_date_format"
             )
 
-    queryset = Schedule.objects.filter(**filters)
-    serializer = ScheduleSerializer(queryset, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    return filters
